d2b7c035fa28b26e5519533d1651b33fb697d84a
Pedro Suttoff <pedronsuttoff@gmail.com>
2026-07-30 22:40:09 -0300

    Adicionando e configurando a classe cadastro

---
+++ src/cadastro.java
@@ -0,0 +1,11 @@
+public class cadastro {
+
+    public void cadastrar(Pessoa pessoa){
+
+        System.out.println("===== CIDADÃO CADASTRADO =====");
+        System.out.println("Nome: " + pessoa.nome);
+        System.out.println("CPF: " + pessoa.cpf);
+        System.out.println("Idade: " + pessoa.idade);
+
+    }
+}
