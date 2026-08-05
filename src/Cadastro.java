aa3cc0f5ff2387fc213d401daaa43dfa4f42b210
Pedro Suttoff <pedronsuttoff@gmail.com>
2026-08-04 22:02:57 -0300

    Adicionando o método Consultar

--- src/Cadastro.java
+++ src/Cadastro.java
@@ -17,4 +17,19 @@ public class Cadastro {
             return true;
         }
 
+        public void consultar(String cpf) {
+
+        for (Pessoa p : pessoas) {
+
+            if(p.cpf.equals(cpf)) {
+
+                System.out.println("\n ====== CIDADÃO ENCONTRADO =======");
+                System.out.println("Nome: " + p.nome);
+                System.out.println("CPF: " + p.cpf);
+                System.out.println("Idade: " + p.idade);
+                return;
+                }
+            }
+        }
+
     }
